b3cfd0bc3e65e95fb1b9b65d6ad73b1c1c078df5
freedarwuin <darwuinjose@gmail.com>
2023-02-26 18:51:13 -0400

    Cambios

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* PHP Mikrotik Billing (https://github.com/freedarwuin/phpdarbill)
+* PHP Mikrotik Billing (https://github.com/freedarwuin/phpdarbill/)
 **/
 
 header('location: ../index.php?_route=admin/');
