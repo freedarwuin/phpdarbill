5baae596d23f1413dbb8c616b6087dd5575336a7
freedarwuin <darwuinjose@gmail.com>
2023-02-26 20:08:02 -0400

    ac

--- admin/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
-* PHP Mikrotik Billing (https://github.com/freedarwuin/phpdarbill/)
-**/
-
-header('location: ../index.php?_route=admin/');
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
-* PHP Mikrotik Billing (https://github.com/freedarwuin/phpdarbill/)
-**/
-
-require ('system/boot.php');
-App::_run();
